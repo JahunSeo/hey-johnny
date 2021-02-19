4416b462fbc3d4e1dc50391c7f5ea76f12c6c501
JahunSeo <wkgjs0809@gmail.com>
2021-02-19 22:47:43 +0900

    [johnny] Asteroid background color

--- src/Field/Asteroid01/index.js
+++ src/Field/Asteroid01/index.js
@@ -72,10 +72,11 @@ export default class Asteroid01 extends Component {
     // check generation initiated
     if (this.generation === undefined) return;
     // draw background
-    ctx.fillStyle = `rgba(255, 235, 199, 1)`;
+    // ctx.fillStyle = `rgba(255, 235, 199, 1)`;
+    ctx.fillStyle = `rgba(255, 255, 255, 1)`;
     ctx.fillRect(0, 0, cvsWidth, cvsHeight);
     // draw generation
-    // this.generation.run(ctx, frameCnt, mouseObj);
+    this.generation.run(ctx, frameCnt, mouseObj);
     // draw screen
     this.screenGroup.run(ctx, frameCnt, mouseObj);
     if (isScreenOn && !isArticleOn && !this.screenGroup.isMoving) {
