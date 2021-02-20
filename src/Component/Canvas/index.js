924a81a7f6ce48ee4e1ef46cdf7440c3badd1157
JahunSeo <wkgjs0809@gmail.com>
2021-02-20 15:56:05 +0900

    [johnny] add click event listener to canvas

--- src/Component/Canvas/index.js
+++ src/Component/Canvas/index.js
@@ -8,6 +8,9 @@ export default class Canvas extends Component {
       mouseX: undefined,
       mouseY: undefined,
       isMouseOverCanvas: false,
+      isMouseMoving: false,
+      clicked: false,
+      clickedFrame: undefined,
     };
   }
 
@@ -17,6 +20,7 @@ export default class Canvas extends Component {
       this._cvs = this.canvasRef.current;
       this._ctx = this._cvs.getContext("2d");
       this._frameCnt = 0;
+      this._cvs.addEventListener("click", this.clickEventHandler);
       // start loop
       console.log("start loop");
       this.loop();
@@ -31,17 +35,37 @@ export default class Canvas extends Component {
     window.cancelAnimationFrame(this._frameId);
     window.removeEventListener("resize", this.resizeEventHandler);
     document.removeEventListener("mousemove", this.mousemoveEventHandler);
+    if (this._cvs) {
+      this._cvs.removeEventListener("click", this.clickEventHandler);
+    }
   }
 
   resizeEventHandler = (event) => {
     // console.log("canvas, resizeEventHandler");
-    // console.log(window.innerWidth, window.innerHeight);
-
     this._cvs.width = window.innerWidth || document.body.clientWidth;
     this._cvs.height = window.innerHeight || document.body.clientHeight;
   };
 
   mousemoveEventHandler = (event) => {
+    // console.log("canvas, mousemove");
+    this.setMouseLocal(event);
+    this.mouseObj.isMouseMoving = true;
+  };
+
+  clickEventHandler = (event) => {
+    // console.log("canvas, mouse click");
+    this.setMouseLocal(event);
+    this.mouseObj.clicked = true;
+    this.mouseObj.clickedFrame = this._frameCnt;
+  };
+
+  resetMouseObj = () => {
+    this.mouseObj.isMouseMoving = false;
+    this.mouseObj.clicked = false;
+    this.mouseObj.clickedFrame = undefined;
+  };
+
+  setMouseLocal = (event) => {
     // https://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
     let rect = this._cvs.getBoundingClientRect();
     let borderWidth = 0; // temp
@@ -62,12 +86,19 @@ export default class Canvas extends Component {
     this.mouseObj.mouseX = mouseX;
     this.mouseObj.mouseY = mouseY;
     this.mouseObj.isMouseOverCanvas = isMouseOverCanvas;
+
+    return {
+      mouseX,
+      mouseY,
+      isMouseOverCanvas,
+    };
   };
 
   loop = () => {
     this._frameCnt += 1;
     let { draw, isLooped = true } = this.props;
     draw(this._ctx, this._frameCnt, this.mouseObj);
+    this.resetMouseObj();
     if (isLooped) {
       this._frameId = window.requestAnimationFrame(this.loop);
     }
