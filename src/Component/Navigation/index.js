54819d660b62a595a78164c5a57da3db483d1b1e
JahunSeo <wkgjs0809@gmail.com>
2021-02-20 20:52:55 +0900

    [johnny] Navigation backbtn styling

--- src/Component/Navigation/index.js
+++ src/Component/Navigation/index.js
@@ -1,41 +1,85 @@
 import React, { Component } from "react";
 import { PAGES } from "../../Constant";
+import { TransitionGroup } from "react-transition-group";
+import { CSSTransitionWrapper, SlowCSSTransitionWrapper } from "../Transition";
 
 import styles from "./index.module.css";
 // import classNames from "classnames/bind";
 // const cx = classNames.bind(styles);
 
 export default class Navigation extends Component {
+  state = {
+    sectionIndex: 0,
+    isBackBtnOn: false,
+  };
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.currentPage !== this.props.currentPage) {
+      if (this.props.currentPage !== PAGES.MAIN) {
+        this.subtractSectionIndex();
+      }
+    }
+  }
+
+  addSectionIndex = () => {
+    this.setState(({ sectionIndex }) => ({ sectionIndex: sectionIndex + 1 }));
+  };
+  subtractSectionIndex = () => {
+    this.setState(({ sectionIndex }) => ({ sectionIndex: sectionIndex - 1 }));
+  };
+
+  showBackBtn = () => {
+    this.setState({
+      isBackBtnOn: true,
+    });
+  };
+
+  handleBackBtn = () => {
+    this.props.setPage(PAGES.MAIN);
+    this.setState((state) => ({
+      isBackBtnOn: false,
+    }));
+  };
+
   render() {
-    // todo: 선택 시 사라지는 애니메이션 실행 후 화면 전환
-    // 게임 만들 때 쓰던 것 활용
-    // 페이지가 메인이 아닐 때는, 돌아가기 버튼만 제시
+    let { sectionIndex, isBackBtnOn } = this.state;
+
     return (
       <nav className={styles.body}>
-        <div
-          className={styles.btn}
-          onClick={() => this.props.setPage(PAGES.MAIN)}
-        >
-          메인
-        </div>
-        <div
-          className={styles.btn}
-          onClick={() => this.props.setPage(PAGES.CANDY)}
-        >
-          별사탕
-        </div>
-        <div
-          className={styles.btn}
-          onClick={() => this.props.setPage(PAGES.QUIZ)}
-        >
-          퀴즈
-        </div>
-        <div
-          className={styles.btn}
-          onClick={() => this.props.setPage(PAGES.WIZLAB)}
-        >
-          위즈랩
-        </div>
+        <TransitionGroup component={null}>
+          {sectionIndex >= 0 && (
+            <SlowCSSTransitionWrapper
+              key={0}
+              appear={true}
+              onEntered={this.addSectionIndex}
+              onExited={this.showBackBtn}
+              wrapClassName={styles.secText}
+            >
+              <p className={styles.greeting}>안녕하세요!</p>
+            </SlowCSSTransitionWrapper>
+          )}
+          {sectionIndex >= 1 && (
+            <CSSTransitionWrapper
+              key={1}
+              appear={true}
+              // onEntered={this.addSectionIndex}
+              onExited={this.subtractSectionIndex}
+              wrapClassName={styles.secText}
+            >
+              <p className={styles.guide}>아래 위성 중 하나를 선택해주세요.</p>
+            </CSSTransitionWrapper>
+          )}
+          {isBackBtnOn && (
+            <SlowCSSTransitionWrapper
+              key={2}
+              appear={true}
+              onExited={this.addSectionIndex}
+              wrapClassName={styles.secText}
+            >
+              <div className={styles.btn} onClick={this.handleBackBtn}></div>
+            </SlowCSSTransitionWrapper>
+          )}
+        </TransitionGroup>
       </nav>
     );
   }
--- src/Component/Transition/index.js
+++ src/Component/Transition/index.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { CSSTransition } from "react-transition-group";
 import SectionTransition from "./SectionTransition.module.css";
+import SlowTransition from "./SlowTransition.module.css";
 
 // https://stackoverflow.com/questions/62187461/using-react-finddomnode-is-deprecated-in-strictmode-is-thrown-as-a-warning-when
 
-export const CSSTransitionWrapper = ({ children, wrapClassName, ...props }) => {
+export const CSSTransitionWrapper = ({
+  children,
+  wrapClassName,
+  classNames,
+  timeout,
+  ...props
+}) => {
   const nodeRef = React.useRef(null);
-  let timeout = {
-    enter: 700,
-    exit: 300,
-  };
+
+  if (timeout === undefined) {
+    timeout = {
+      enter: 700,
+      exit: 300,
+    };
+  }
+  if (classNames === undefined) {
+    classNames = SectionTransition;
+  }
 
   return (
     <CSSTransition
@@ -22,5 +35,15 @@ export const CSSTransitionWrapper = ({ children, wrapClassName, ...props }) => {
         {children}
       </div>
     </CSSTransition>
+  );
+};
+
+export const SlowCSSTransitionWrapper = (props) => {
+  return (
+    <CSSTransitionWrapper
+      timeout={{ enter: 1000, exit: 1000 }}
+      classNames={SlowTransition}
+      {...props}
+    />
   );
 };
