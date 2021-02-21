4809d9585fb7e28693460094ae3a1dae2487c43d
JahunSeo <wkgjs0809@gmail.com>
2021-02-22 02:56:01 +0900

    [johnny] delete debug code

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
     window.addEventListener("resize", this.resizeEventHandler);
     this.resizeEventHandler();
     // // DEBUG // //
-    this.setPage(PAGES.XOR);
+    // this.setPage(PAGES.XOR);
   }
 
   componentWillUnmount() {
